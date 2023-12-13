7608f64a232dd67cd4da4b9de2d3640dbba13046
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-13 19:57:40 +0300

    link mfor my github profile

--- qrCodeGenenrator.py
+++ qrCodeGenenrator.py
@@ -1,6 +1,6 @@
 import qrcode
 
 features = qrcode.QRCode(version=1, box_size=30, border = 3)
-features.add_data()
+features.add_data('github.com/amcoolalphonce')
 generated_image = qrcode.make("AmcoolGithub")
 generated_image.save('image1.png')
