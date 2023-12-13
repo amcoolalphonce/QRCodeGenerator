606c65d1ca2a7723c29a58402d5f7fb51ad57d70
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-13 19:55:46 +0300

    Border

--- qrCodeGenenrator.py
+++ qrCodeGenenrator.py
@@ -1,5 +1,5 @@
 import qrcode
 
-features = qrcode.QRCode(version=1, box_size=30)
+features = qrcode.QRCode(version=1, box_size=30, border = 3)
 generated_image = qrcode.make("AmcoolGithub")
 generated_image.save('image1.png')
