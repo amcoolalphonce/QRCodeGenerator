5680e21798f99f759aebd85d2eb62e1965293a0a
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-13 19:59:14 +0300

    All data to fit in the code

--- qrCodeGenenrator.py
+++ qrCodeGenenrator.py
@@ -2,5 +2,6 @@ import qrcode
 
 features = qrcode.QRCode(version=1, box_size=30, border = 3)
 features.add_data('github.com/amcoolalphonce')
-generated_image = qrcode.make("AmcoolGithub")
+features.make(fit=True)
+generated_image = qrcode.make("Amcool Github")
 generated_image.save('image1.png')
