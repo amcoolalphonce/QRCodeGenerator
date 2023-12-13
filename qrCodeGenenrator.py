4047f2cc156dd3b82289d90eccf12f52c08a5802
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-13 20:05:51 +0300

    red

--- qrCodeGenenrator.py
+++ qrCodeGenenrator.py
@@ -3,5 +3,5 @@ import qrcode
 features = qrcode.QRCode(version=1, box_size=30, border = 3)
 features.add_data('github.com/amcoolalphonce')
 features.make(fit=True)
-generated_image = features.make_image(fill_color = "blue",back_color ="white")
-generated_image.save('image3.png')
+generated_image = features.make_image(fill_color = "red",back_color ="white")
+generated_image.save('image4.png')
